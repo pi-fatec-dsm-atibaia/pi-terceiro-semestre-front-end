eca46db677bc6fbf887700e2cfd18460796c64da
Iago Homsi <iagohomsi@gmail.com>
2025-11-08 19:56:22 -0300

    Salvando modificações locais antes do merge

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Login() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
       <main className="bg-amber-50">Essa será a página de login</main>
     </div>
   );
